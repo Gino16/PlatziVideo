53aabfdd189d5c2fe39cfa69e98e2d0d1871df1b
Gino Ascencio <gino.francisco@hotmail.com>
2022-02-12 09:24:45 -0500

    ADD Props

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import './../assets/styles/components/CarouselItem.scss';
 
 import playIcon from '../assets/static/play-icon.png';
@@ -19,5 +20,13 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     </div>
   </div>
 );
+
+CarouselItem.propTypes = {
+  cover: propTypes.string,
+  title: propTypes.string,
+  year: propTypes.number,
+  contentRating: propTypes.string,
+  duration: propTypes.number
+}
 
 export default CarouselItem;
