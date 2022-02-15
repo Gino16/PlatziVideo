e4194c1ca04f3e2fd1abb3baab297018b9b4d65d
Gino Ascencio <gino.francisco@hotmail.com>
2022-02-15 09:13:49 -0500

    ADD Register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Register.scss';
 
 function Register() {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
   return (
     <section className='register'>
       <section className='register__container'>
         <h2>Regístrate</h2>
-        <form className='register__container--form'>
-          <input className='input' type='text' placeholder='Nombre' />
-          <input className='input' type='text' placeholder='Correo' />
-          <input className='input' type='password' placeholder='Contraseña' />
-          <button className='button' type='button'>Registrarme</button>
+        <form className='register__container--form' onSubmit={handleSubmit}>
+          <input className='input' name='name' type='text' placeholder='Nombre' onChange={handleInput} />
+          <input className='input' name='email' type='text' placeholder='Correo' onChange={handleInput} />
+          <input className='input' name='password' type='password' placeholder='Contraseña' onChange={handleInput} />
+          <button className='button' type='submit'>Registrarme</button>
         </form>
         <Link to='/login'>Iniciar sesión</Link>
       </section>
